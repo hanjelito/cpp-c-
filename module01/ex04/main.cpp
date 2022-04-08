33efb3aa4ed264e160742b139c99a74d6b8db914
juan-gon <jgonzales@peruyoung.com>
2022-04-09 01:18:19 +0200

    ajustes

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,24 +1,68 @@
+#include <fstream>
+#include <iostream>
+#include <string>
 
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::ifstream;
+using std::ios;
+using std::ofstream;
+using std::size_t;
+using std::string;
 
-#include "Weapon.hpp"
-#include "HumanA.hpp"
-#include "HumanB.hpp"
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        cerr << "./sed <filename> <s1> <s2>" << endl;
 
-int main()
-{
-	{
-		Weapon club = Weapon("crude spiked club");
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
+        return 1;
+    }
+
+    string infile = argv[1];
+    ifstream fin;
+    fin.open(infile, ios::in);
+
+    if (!fin) {
+        cerr << "error: unable to open " << infile << endl;
+
+        return 2;
+    }
+
+    string outfile = infile + ".replace";
+    ofstream fout;
+    fout.open(outfile, ios::out | ios::trunc);
+
+    if (!fout) {
+        cerr << "error: unable to open " << outfile << endl;
+
+        return 3;
+    }
+
+    string tmp = "";
+    string input = "";
+
+    while (!fin.eof()) {
+        getline(fin, tmp);
+        input += tmp;
+
+        if (!fin.eof())
+            input += '\n';
+    }
+
+    fin.close();
+    string s1 = argv[2];
+    string s2 = argv[3];
+    size_t pos = input.find(s1);
+
+    while (pos != string::npos) {
+        input.erase(pos, s1.length());
+        input.insert(pos, s2);
+        pos = input.find(s1, pos + s2.length());
+    }
+
+    fout << input;
+    fout.close();
+
+    return 0;
 }

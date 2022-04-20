83ad4867888fee31b6daf4e1bf342a90819e2ab3
juan-gon <jgonzales@peruyoung.com>
2022-04-21 01:06:00 +0200

    ajustes

---
+++ module03/ex02/ClapTrap.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-gon <juan-gon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 22:58:44 by juan-gon          #+#    #+#             */
+/*   Updated: 2022/04/19 20:53:29 by juan-gon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class ClapTrap
+{
+	protected:
+		string	_Name;
+		int		_HitPoints;
+		int		_EnergyPoints;
+		int		_AttackDamage;
+	public:
+		ClapTrap( void );
+		ClapTrap( const string Name);
+		ClapTrap( const ClapTrap &claptrap);
+		ClapTrap &operator=(const ClapTrap &claptrap );
+		~ClapTrap( void );
+		//
+		void	attack(string const &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+		//
+		void	setName(string Name);
+		void	setHitPoints(int HitPoints);
+		void	setEnergyPoints(int EnergyPoints);
+		void	setAttackDamage(int AttackDamage);
+		//
+		string	getName( void );
+		int 	getHitPoints( void );
+		int 	getEnergyPoints( void );
+		int		getAttackDamage( void );
+};
+
+#endif
---
+++ module03/ex02/FragTrap.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-gon <juan-gon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 10:14:03 by juan-gon          #+#    #+#             */
+/*   Updated: 2022/04/21 01:04:02 by juan-gon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap( void )
+{
+	this->_Name			= "";
+	this->_HitPoints	= 100;
+	this->_EnergyPoints	= 100;
+	this->_AttackDamage	= 30;
+	cout << "\033[1;32m" << "FragTrap " << " was constructed no name" << "\033[0;37m" << endl;
+}
+
+FragTrap::FragTrap(string const &Name):ClapTrap::ClapTrap(Name)
+{
+	this->_HitPoints	= 100;
+	this->_EnergyPoints	= 100;
+	this->_AttackDamage	= 30;
+	cout << "\033[1;32m" << "FragTrap " << " was constructed" << "\033[0;37m" << endl;
+}
+
+FragTrap::FragTrap(FragTrap const &fragtrap)
+{
+	 this->operator=(fragtrap);
+	 cout << "\033[1;32m" << "FragTrap " << this->_Name << " copy constructor" << "\033[0;37m" << endl;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &fragtrap)
+{
+	this->_Name         = fragtrap._Name;
+    this->_HitPoints    = fragtrap._HitPoints;
+    this->_EnergyPoints = fragtrap._EnergyPoints;
+    this->_AttackDamage = fragtrap._AttackDamage;
+	cout << "\033[1;32m" << "FragTrap " << this->_Name << " Assignation constructor" << "\033[0;37m" << endl;
+	return (*this);
+}
+
+FragTrap::~FragTrap()
+{
+	cout << "\033[1;32m" << "FragTrap is destroyed" << "\033[0;37m" << endl;
+}
+
+void FragTrap::highFivesGuys(void)
+{
+	cout << "\033[1;32m" << "FragTrap " << this->getName() << " High five!!!" << "\033[0;37m" << endl;
+}

a70617bd24dfd9c716aa8fe13f8d83fc99f89db8
juan-gon <jgonzales@peruyoung.com>
2022-07-25 02:27:23 +0200

    ajustes

--- module08/ext01/main.cpp
+++ module08/ext01/main.cpp
@@ -6,9 +6,42 @@
 /*   By: juan-gon <juan-gon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 12:06:00 by juan-gon          #+#    #+#             */
-/*   Updated: 2022/07/13 21:40:56 by juan-gon         ###   ########.fr       */
+/*   Updated: 2022/07/25 02:12:17 by juan-gon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "whatever.hpp"
+#include "span.hpp"
 
+Span::Span( void ): _size(0)
+{}
+
+Span::Span(int unsigned num): _size(num)
+{}
+
+Span::Span( Span const &o)
+{
+	*this = o;
+}
+
+Span &Span::operator=( Span const &o)
+{
+	if(this != &o)
+	{
+		this->_size = o._size;
+		this->_container.clear();
+		this->_container = o._container;
+	}
+	return (*this);
+}
+
+Span::~Span( void )
+{
+	this->_container.size();
+}
+
+void Span::addNumber(int num)
+{
+	if (this->_container.size() == this->_size)
+		throw Span::FilledUp();
+	this->_container.push_back(num);
+}
--- module08/ext01/span.hpp
+++ module08/ext01/span.hpp
@@ -6,15 +6,47 @@
 /*   By: juan-gon <juan-gon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 11:53:15 by juan-gon          #+#    #+#             */
-/*   Updated: 2022/07/16 11:55:56 by juan-gon         ###   ########.fr       */
+/*   Updated: 2022/07/25 02:25:56 by juan-gon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SNAP_HPP
-#define SNAP_HPP
+#ifndef SPAN_H
+#define SPAN_H
 
-#include <iosatream>
+#include <iostream>
+#include <algorithm>
+#include <exception>
+#include <string>
+#include <vector>
+#include <list>
 
+class Span
+{
+	private:
+		unsigned int _size;
+		std::vector<int> _container;
+		Span( void );
+	public:
+		Span(int unsigned num);
+		Span( Span const &o);
+		Span &operator=( Span const &o);
+		~Span( void );
+		//
+		void addNumber(int num);
+		int shortestSpan( void );
+		int longestSpan( void );
+		//
+		class SearchFailed : public std::exception
+		{
+			public:
+					virtual const char *what() const throw ();
+		};
 
+		class FilledUp : public std::exception
+		{
+			public:
+					virtual const char *what() const throw ();
+		};
+};
 
 #endif
